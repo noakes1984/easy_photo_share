3445c8fb4ff1981025bb9f4fb8411ab4be0111db
noakes1984 <noakes1984@gmail.com>
2021-07-23 17:50:39 -0500

    expo auth expo building

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import DropboxComp from "./src/DropboxComp";
+import { authorize } from "react-native-app-auth";
+import * as WebBrowser from "expo-web-browser";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { Button, Platform } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: "https://www.dropbox.com/oauth2/authorize",
+  tokenEndpoint: "https://www.dropbox.com/oauth2/token",
+};
 
 export default function App() {
+  const useProxy = Platform.select({ web: false, default: true });
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "foz4e43qesa077j",
+      // There are no scopes so just pass an empty array
+      scopes: [],
+      // Dropbox doesn't support PKCE
+      usePKCE: false,
+      // For usage in managed apps using the proxy
+      redirectUri: makeRedirectUri({
+        scheme: "http://localhost:3000/",
+        useProxy
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+    }
+
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Login"
+        onPress={() => {
+          promptAsync({useProxy});
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +55,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
